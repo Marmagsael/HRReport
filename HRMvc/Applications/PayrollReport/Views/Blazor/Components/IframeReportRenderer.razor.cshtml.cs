2e127f6f99336f11417dcb06114b22b5f1e1915b
Mar Lopez Magsael <marmagsael@gmail.com>
2026-01-09 17:37:33 +0800

    -

---
+++ HRMvc/Applications/PayrollReport/Views/Blazor/Components/IframeReportRenderer.razor.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HRMvc.Applications.PayrollReport.Views.Blazor.Components;
+
+public class IframeReportRenderer_razor : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
