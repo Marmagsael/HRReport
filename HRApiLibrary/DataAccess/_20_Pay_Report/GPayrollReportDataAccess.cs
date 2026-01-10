b7c4d5549e636faa084a022bddec3983ec080fb0
Mar Lopez Magsael <marmagsael@gmail.com>
2026-01-11 03:42:58 +0800

    -

---
+++ HRApiLibrary/DataAccess/_20_Pay_Report/GPayrollReportDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using HRApiLibrary.DataAccess._90_Utils.Interface;
+using HRApiLibrary.Models._20_PayGeneric;
+using System.Security.AccessControl;
+
+namespace HRApiLibrary.DataAccess._20_Pay_Report;
+
+public class GPayrollReportDataAccess : IGPayrollReportDataAccess
+{
+
+    private readonly I_90_001_MySqlDataAccess _sql;
+
+    public GPayrollReportDataAccess(I_90_001_MySqlDataAccess sql)
+    {
+        _sql = sql;
+    }
+
+    public async Task<List<GChartofacctModel?>?> _02s(string schema, string conn)
+    {
+        string sql = $@"select  * from {schema}.Chartofacct order by Type Desc, AcctName ";
+        var data = await _sql.FetchData<GChartofacctModel?, dynamic>(sql, new { }, conn);
+        return data;
+    }
+    public async Task<List<GChartofacctModel?>?> _02ByAcctTypes(string acctType, string schema, string conn)
+    {
+        string sql  = $@"select  * from {schema}.Chartofacct where AcctType = @AcctType order by AcctName ";
+        var data    = await _sql.FetchData<GChartofacctModel?, dynamic>(sql, new { AcctType = acctType }, conn);
+        return data;
+    }
+
+
+}
+
+
+public interface IGPayrollReportDataAccess
+{
+    Task<List<GChartofacctModel?>?> _02ByAcctTypes(string acctType, string schema, string conn);
+    Task<List<GChartofacctModel?>?> _02s(string schema, string conn);
+}
