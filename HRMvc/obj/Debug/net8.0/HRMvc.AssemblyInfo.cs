5b5fa2fd86b20c69aa5ef85e6e5a72b0be68c85e
Mar Lopez Magsael <marmagsael@gmail.com>
2026-01-16 14:33:43 +0800

    -

--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07ab1dfa01d4b478ae4cf744975b639167d6c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8051014b2da02ca0f0168b24aabd424a1a642279")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
