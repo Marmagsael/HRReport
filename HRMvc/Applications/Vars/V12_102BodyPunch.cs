ae1600b729d38c59df7f983815e1997d1bcfbc55
Mar Lopez Magsael <marmagsael@gmail.com>
2026-01-27 22:55:58 +0800

    -

---
+++ HRMvc/Applications/Vars/V12_102BodyPunch.cs
@@ -0,0 +1,20 @@
+using System;
+using HRApiLibrary.Models._10_Pis.OPis;
+
+namespace HRMvc.Applications.Vars;
+
+public class V12_102BodyPunch
+{
+    public OempmasModel?    empmas              {get; set;} = new OempmasModel();
+    public string?          currIn              {get; set;} = "-"; 
+    public string?          currOut             {get; set;} = "-"; 
+    public string?          prevIn              {get; set;} = "00:00"; 
+    public string?          prevOut             {get; set;} = "00:00"; 
+    public bool             currInDisabled      {get; set;} = false;
+    public  bool            currOutDisabled     {get; set;} = false;
+    public  bool            prevInDisabled      {get; set;} = false; 
+    public  bool            prevOutDisabled     {get; set;} = false;
+    public  string?         msg                 {get; set;} = "Punch In for  : ";
+    public  string?         msgVal              {get; set;} = DateTime.Now.ToString("yyyy-MM-dd");
+
+}
--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9628ac2797b9611e0272d7099da649efded206")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffdd77ee1f97143fb7349097c46c209d78da8f2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
