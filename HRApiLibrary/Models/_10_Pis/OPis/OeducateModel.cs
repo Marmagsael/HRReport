8338ae8eecd3b5fa85077a9523c6a0c1282e2b88
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-04 13:32:59 +0800

    -

---
+++ HRApiLibrary/Models/_10_Pis/OPis/OeducateModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HRApiLibrary.Models._10_Pis.OPis;
+
+public class OeducateModel
+{
+    public string? EMPNUMBER { get; set; }
+    public string? CODE { get; set; }
+    public string? SCHOOL { get; set; }
+    public string? FROM_ { get; set; }
+    public string? TO_ { get; set; }
+    public string? COURSE { get; set; }
+    public string? LEVEL { get; set; }
+}
---
+++ HRApiLibrary/Models/_10_Pis/OPis/OemergencModel.cs
@@ -0,0 +1,11 @@
+namespace HRApiLibrary.Models._10_Pis.OPis;
+
+public class OemergencModel
+{
+    public string? EMPNUMBER { get; set; }
+    public string? NAME { get; set; }
+    public string? ADDR { get; set; }
+    public string? RELA { get; set; }
+    public string? TEL { get; set; }
+}
+
