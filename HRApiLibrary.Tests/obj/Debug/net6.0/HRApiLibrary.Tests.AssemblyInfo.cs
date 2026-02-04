67844bee29cd9ada3d3cf63ac8f64e79434cce19
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-04 11:17:37 +0800

    -

--- HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
+++ HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+441f56eb9997e5569055552d7b537a7d10385538")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17508256ec3e3115c5ed8049767fd7d2d00fec5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92c8699950b21aea31ea8a27983a1597c9d796d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17508256ec3e3115c5ed8049767fd7d2d00fec5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
