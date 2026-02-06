4f49fd165dcb9cc082bd9aed725ab82445d705df
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-06 21:16:28 +0800

    -

--- HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
+++ HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b04865eecf9b203b8a0f078d0b70400e71c32cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40366640551b7b3364d0137fc252c5a44f5703be")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
