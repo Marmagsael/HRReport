a301ada694b3a52b9c8b60f30fe4a9f43d4c9b55
judithlorrenreyes <jlrproj@gmail.com>
2026-02-09 08:24:19 +0800

    Update all modules in 201 records

--- HRApiLibrary/DataAccess/_10_Pis/OPis/OempmasDataAccess.cs
+++ HRApiLibrary/DataAccess/_10_Pis/OPis/OempmasDataAccess.cs
@@ -51,7 +51,7 @@ public class OempmasDataAccess : IOempmasDataAccess
 
     public async Task<List<OempmasModel?>?> _02(string empnumber, string schema, string conn)
     {
-        var sql = $@"select  s.name EmpStatus, p.name PositionName, e.*  from {schema}.Empmas e
+        var sql = $@"select  s.name EmpStatus, p.name PositionName, e.* from {schema}.Empmas e
                  left join {schema}.position p on p.code = e.position_
                  left join {schema}.empstat s on s.code = e.empstat_                              
                  where e.Empnumber = @Empnumber";
