f576d99c20dd13cdf7b6fa0f14098641d68a91f9
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-09 09:05:33 +0800

    -

--- HRMvc/StartupConfig/SessionService.cs
+++ HRMvc/StartupConfig/SessionService.cs
@@ -1,13 +1,15 @@
-using Microsoft.AspNetCore.Http;
-
 namespace HRMvc.StartupConfig;
+
 public class SessionService
 {
     private readonly IHttpContextAccessor _http;
 
-    public SessionService(IHttpContextAccessor http) { _http = http; }
+    public SessionService(IHttpContextAccessor http)
+    {
+        _http = http;
+    }
 
-    public string? OldPis => _http.HttpContext?.Session.GetString("OldPisDb");
-    public string? OldPay => _http.HttpContext?.Session.GetString("OldPayDb");
+    public string? OldPis => _http.HttpContext?.Session.GetString("OldPis");
+    public string? OldPay => _http.HttpContext?.Session.GetString("OldPay");
     public string? EmpNumber => _http.HttpContext?.Session.GetString("EmpNumber");
 }
