cb5b743073c3c8504699e774b43f8bf2f530b270
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-13 01:03:55 +0800

    -

---
+++ HRApiLibrary/DataAccess/_10_Pis/OPis/OReferDataAccess.cs
@@ -0,0 +1,68 @@
+﻿using HRApiLibrary.DataAccess._90_Utils.Interface;
+using HRApiLibrary.Models._10_Pis.OPis;
+
+
+namespace HRApiLibrary.DataAccess._10_Pis.OPis
+{
+    public class OReferDataAccess : IOReferDataAccess
+    {
+
+        private readonly I_90_001_MySqlDataAccess _sql;
+
+        public OReferDataAccess(I_90_001_MySqlDataAccess sql)
+        {
+            _sql = sql;
+        }
+
+        public async Task<OReferModel?> _01(OReferModel refer, string schema, string conn)
+        {
+            string sql = $@"Insert into {schema}.Refer (EMPNUMBER, NAME, ADDR, TEL, POSITION) values (@EMPNUMBER, @NAME, @ADDR, @TEL, @POSITION)";
+            await _sql.ExecuteCmd<dynamic>(sql, refer, conn);
+
+            sql = $@"SELECT * FROM {schema}.Refer WHERE ID = (SELECT @@IDENTITY)";
+
+            var res = await _sql.FetchData<OReferModel?, dynamic>(sql, new { }, conn);
+
+            return res.FirstOrDefault();
+        }
+
+
+        public async Task<List<OReferModel?>?> _02(string empnumber, string schema, string conn)
+        {
+            string sql = $@"select  EMPNUMBER, NAME, ADDR, TEL, POSITION from {schema}.Refer where Empnumber = @Empnumber";
+            var data = await _sql.FetchData<OReferModel?, dynamic>(sql, new { Empnumber = empnumber }, conn);
+            return data;
+        }
+
+
+        public async Task<OReferModel?> _03(int id, OReferModel refer, string schema, string conn)
+        {
+            string sql = $@"Update {schema}.Refer set EMPNUMBER = @EMPNUMBER, NAME = @NAME, ADDR = @ADDR, TEL = @TEL, POSITION = @POSITION where Id = @Id;";
+            await _sql.ExecuteCmd<dynamic>(sql, refer, conn);
+
+            sql = $@" select  * from {schema}.Refer x where x.Id = @Id ;";
+            var data = await _sql.FetchData<OReferModel?, dynamic>(sql, new { Id = id }, conn);
+            return data?.FirstOrDefault();
+        }
+
+        public async Task<OReferModel?> _04(int id, string schema, string conn)
+        {
+            string sql = $@"Delete from {schema}.Refer where Id = @Id;";
+            await _sql.ExecuteCmd<dynamic>(sql, new { Id = id }, conn);
+
+            sql = $@" select  * from {schema}.Refer x where x.Id = @Id ;";
+            var data = await _sql.FetchData<OReferModel?, dynamic>(sql, new { Id = id }, conn);
+            return data?.FirstOrDefault();
+        }
+
+    }
+}
+
+
+public interface IOReferDataAccess
+{
+    Task<OReferModel?> _01(OReferModel refer, string schema, string conn);
+    Task<List<OReferModel?>?> _02(string empnumber, string schema, string conn);
+    Task<OReferModel?> _03(int id, OReferModel refer, string schema, string conn);
+    Task<OReferModel?> _04(int id, string schema, string conn);
+}
