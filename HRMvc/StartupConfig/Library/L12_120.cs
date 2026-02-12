9b3a14774f1e77ab6c4ee311314cf875609ff219
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-12 17:09:42 +0800

    -

--- HRMvc/StartupConfig/Library/L12_120.cs
+++ HRMvc/StartupConfig/Library/L12_120.cs
@@ -6,19 +6,19 @@ namespace HRMvc.StartupConfig.Library;
 
 public class L12_120
 {
-    private readonly IOEmpmasDataAccess _oempmas;
+    private readonly IOEmpmasDataAccess _OEmpmas;
     private readonly IHttpContextAccessor _http;
 
-    public L12_120(IOEmpmasDataAccess oempmas, IHttpContextAccessor http)
+    public L12_120(IOEmpmasDataAccess OEmpmas, IHttpContextAccessor http)
     {
-        _oempmas = oempmas;
+        _OEmpmas = OEmpmas;
         _http = http;
     }
 
 
-    public async Task<List<OempmasModel?>> Oempmass(UserClaimsModel uc)
+    public async Task<List<OEmpmasModel?>> OEmpmass(UserClaimsModel uc)
     {
-        List<OempmasModel?>? oEmpmass = [];
+        List<OEmpmasModel?>? OEmpmass = [];
         // var pisdb = _http.HttpContext?.Session.GetString("OldPis");
         // var paydb = _http.HttpContext?.Session.GetString("OldPay");
         // var conn = uc.Conn;
@@ -28,8 +28,8 @@ public class L12_120
         var pisdb = uc.OpisDb; 
         var conn = uc.Conn;
         
-        var empass = await _oempmas._02(empnumber,pisdb, conn);
+        var empass = await _OEmpmas._02(empnumber,pisdb, conn);
         
-        return oEmpmass;
+        return OEmpmass;
     }
 }
