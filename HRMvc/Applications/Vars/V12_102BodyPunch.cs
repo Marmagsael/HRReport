7310a9a07a44327a17eb272fd79aab81210591c8
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-13 10:13:49 +0800

    -

--- HRMvc/Applications/Vars/V12_102BodyPunch.cs
+++ HRMvc/Applications/Vars/V12_102BodyPunch.cs
@@ -5,7 +5,6 @@ namespace HRMvc.Applications.Vars;
 
 public class V12_102BodyPunch
 {
-<<<<<<< HEAD
     public OEmpmasModel?    Empmas              {get; set;} = new OEmpmasModel();
     public string?          CurrIn              {get; set;} = "-"; 
     public string?          CurrOut             {get; set;} = "-"; 
@@ -17,18 +16,5 @@ public class V12_102BodyPunch
     public  bool            PrevOutDisabled     {get; set;} = false;
     public  string?         Msg                 {get; set;} = "Punch In for  : ";
     public  string?         MsgVal              {get; set;} = DateTime.Now.ToString("yyyy-MM-dd");
-=======
-    public OEmpmasModel?    empmas              {get; set;} = new OEmpmasModel();
-    public string?          currIn              {get; set;} = "-"; 
-    public string?          currOut             {get; set;} = "-"; 
-    public string?          prevIn              {get; set;} = "00:00"; 
-    public string?          prevOut             {get; set;} = "00:00"; 
-    public bool             currInDisabled      {get; set;} = false;
-    public  bool            currOutDisabled     {get; set;} = false;
-    public  bool            prevInDisabled      {get; set;} = false; 
-    public  bool            prevOutDisabled     {get; set;} = false;
-    public  string?         msg                 {get; set;} = "Punch In for  : ";
-    public  string?         msgVal              {get; set;} = DateTime.Now.ToString("yyyy-MM-dd");
->>>>>>> DA_mar
 
 }
