de2a6cf591eb0e861650624dbbb36a74916bffb9
Abraham Kevin Gagui <abraham_gagui@gervasioph.com>
2026-02-16 15:30:47 +0800

    -

--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f19163f0607305c970468a81729ff700e1aa6aa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423ab5a1f4182a43015d1095df3f540eb95bc28c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
