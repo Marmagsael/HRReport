bff1a1e7913e04c2e422cd26eb09c79a4e9fa4c9
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-17 11:56:28 +0800

    -

--- HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
+++ HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4135368be4f07507ba7b229c2962a6dcd8477f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f91438f6b00f7bd3f9309a49686247cb0d4c7f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HRMvc/Applications/PisReport/Vars/V2103Model.cs
@@ -0,0 +1,12 @@
+namespace HRMvc.Applications.PisReport.Vars;
+
+public class V2103Model
+{
+    public string   EmpNumber       { get; set; } = ""; 
+    public string   EmpName         { get; set; } = ""; 
+    public string   ClName          { get; set; } = ""; 
+    public string   EmpStat         { get; set; } = ""; 
+    public string   StatusName      { get; set; } = ""; 
+    
+
+}
