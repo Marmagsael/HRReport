870b0f3e865bfe5ec5492225c22a76fa19a9759c
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-21 16:07:13 +0800

    -

--- HRApiLibrary/DataAccess/_10_Pis/AttreqhdrDataAccess.cs
+++ HRApiLibrary/DataAccess/_10_Pis/AttreqhdrDataAccess.cs
@@ -11,36 +11,70 @@ public class AttreqhdrDataAccess : IAttreqhdrDataAccess
         _sql = sql;
     }
 
-    public async Task<AttreqhdrModel?> _01(AttreqhdrModel attreqhdr, string schema, string conn)
-    {
-        string sql = $@"Insert into {schema}.Attreqhdr (UserId, EmpNumber, DateRequested, Remarks, Status, UserId_FApprover) values (@UserId, @EmpNumber, @DateRequested, @Remarks, @Status, @UserId_FApprover)";
-        await _sql.ExecuteCmd<dynamic>(sql, attreqhdr, conn);
+    public async Task<AttreqhdrModel?> _01(AttreqhdrModel attreqhdr, string schema, string conn )
+	{
+		string sql = $@"Insert into {schema}.Attreqhdr 
+                            (UserId,  EmpNumber,  DateRequested,  CovStart,  CovEnd,  AttReqTypeId,  Remarks,  Status,  EmpNumber_Approver,  TotHrs) values 
+                            (@UserId, @EmpNumber, @DateRequested, @CovStart, @CovEnd, @AttReqTypeId, @Remarks, @Status, @EmpNumber_Approver, @TotHrs)" ; 
+		await _sql.ExecuteCmd<dynamic>(sql, attreqhdr, conn);
+		sql = $@"SELECT * FROM {schema}.Attreqhdr WHERE ID = (SELECT @@IDENTITY)"; 
+		var res = await _sql.FetchData<AttreqhdrModel?,dynamic>(sql,new { },conn);
 
-        sql = $@"SELECT * FROM {schema}.Attreqhdr WHERE ID = (SELECT @@IDENTITY)";
-
-        var res = await _sql.FetchData<AttreqhdrModel?, dynamic>(sql, new { }, conn);
-
-        return res.FirstOrDefault();
-    }
+		return res.FirstOrDefault();
+	}
 
 
     public async Task<List<AttreqhdrModel?>> _02s(int id, string schema, string conn)
     {
-        string sql = $@"select  Id, UserId, EmpNumber, DateRequested, Remarks, Status, UserId_FApprover from {schema}.Attreqhdr where Id = @Id";
+        string sql = $@"select  Id, UserId, EmpNumber, DateRequested, Remarks, Status, EmpNumber_Approver from {schema}.Attreqhdr where Id = @Id";
         var data = await _sql.FetchData<AttreqhdrModel?, dynamic>(sql, new { Id = id }, conn);
         return data ?? [];
     }
 
 
-    public async Task<AttreqhdrModel?> _03(int id, AttreqhdrModel attreqhdr, string schema, string conn)
-    {
-        string sql = $@"Update {schema}.Attreqhdr set UserId = @UserId, EmpNumber = @EmpNumber, DateRequested = @DateRequested, Remarks = @Remarks, Status = @Status, UserId_FApprover = @UserId_FApprover where Id = @Id;";
-        await _sql.ExecuteCmd<dynamic>(sql, attreqhdr, conn);
+    public async Task<AttreqhdrModel?> _03(int id,AttreqhdrModel attreqhdr, string schema, string conn)
+	{
+		string sql = $@"Update {schema}.Attreqhdr set 
+                            DateRequested       = @DateRequested, 
+                            CovStart            = @CovStart, 
+                            CovEnd              = @CovEnd, 
+                            Remarks             = @Remarks, 
+                            EmpNumber_Approver  = @EmpNumber_Approver
+                        where Id = @Id;"; 
+		await _sql.ExecuteCmd<dynamic>(sql, attreqhdr, conn);
+		
+		sql = $@" select  * from {schema}.Attreqhdr x where x.Id = @Id ;";
+		var data = await _sql.FetchData<AttreqhdrModel?, dynamic>(sql, new { Id = id }, conn);
+		return data?.FirstOrDefault();
+	}
+    
+    public async Task<AttreqhdrModel?> _03SendForApproval(AttreqhdrModel attreqhdr, string schema, string conn)
+	{
+        string sql = $@"Update {schema}.Attreqhdr set 
+                            DateRequested       = @DateRequested, 
+                            CovStart            = @CovStart, 
+                            CovEnd              = @CovEnd, 
+                            Remarks             = @Remarks, 
+                            EmpNumber_Approver  = @EmpNumber_Approver, 
+                            Status              = 'F' 
+                        where Id = @Id;
+                        select  * from {schema}.Attreqhdr x where x.Id = @Id ;"; 
+		var data = await _sql.FetchData<AttreqhdrModel?, dynamic>(sql, attreqhdr, conn);
 
-        sql = $@" select  * from {schema}.Attreqhdr x where x.Id = @Id ;";
-        var data = await _sql.FetchData<AttreqhdrModel?, dynamic>(sql, new { Id = id }, conn);
-        return data?.FirstOrDefault();
-    }
+        // *****************************************************************************************************
+        AttreqhistModel attreqhist = new() { AttReqHdrId = attreqhdr.Id, DActionTaken = DateTime.Now, 
+                                             Remarks = attreqhdr.Remarks??"For Approval", SetStatusTo = "F" }; 
+
+        var sql1 =  $@"Insert into {schema}.Attreqhist 
+                            (AttReqHdrId,  DActionTaken,  SetStatusTo,  Remarks) values 
+                            (@AttReqHdrId, @DActionTaken, @SetStatusTo, @Remarks)" ; 
+		await _sql.ExecuteCmd<dynamic>(sql1, attreqhist, conn);
+        // *****************************************************************************************************
+
+
+
+		return data?.FirstOrDefault();
+	}
 
     public async Task<AttreqhdrModel?> _04(int id, string schema, string conn)
     {
@@ -58,5 +92,6 @@ public interface IAttreqhdrDataAccess
     Task<AttreqhdrModel?> _01(AttreqhdrModel attreqhdr, string schema, string conn);
     Task<List<AttreqhdrModel?>> _02s(int id, string schema, string conn);
     Task<AttreqhdrModel?> _03(int id, AttreqhdrModel attreqhdr, string schema, string conn);
+    Task<AttreqhdrModel?> _03SendForApproval(AttreqhdrModel attreqhdr, string schema, string conn); 
     Task<AttreqhdrModel?> _04(int id, string schema, string conn);
 }
