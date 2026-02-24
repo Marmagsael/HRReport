98e06a78a0d34c2800c9c12959cfefc53c9eb8b1
Abraham Kevin Gagui <abraham_gagui@gervasioph.com>
2026-02-24 16:02:02 +0800

    _2217_LE

--- HRMvc/Applications/PisReport/Vars/V2217Model.cs
+++ HRMvc/Applications/PisReport/Vars/V2217Model.cs
@@ -3,7 +3,7 @@ public class V2217Model
 {
     public string? EmpNumber    { get; set; }="";
     public string? EmpName      { get; set; }="";
-    public string? EmpStatus      { get; set; }="";
+    public string? EmpStatus     { get; set; }="";
     public DateTime? LicExpire    { get; set; } 
     public string? SecLicense       { get; set; }="";
     public string? ClName       { get; set; }="";
