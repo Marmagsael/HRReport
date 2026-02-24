703d0b5ee439f7bec68c4a5a5270ca40eaae0584
judithlorrenreyes <jlrproj@gmail.com>
2026-02-24 09:52:12 +0800

    -

--- HRMvc/Applications/PisReport/Vars/V2212Model.cs
+++ HRMvc/Applications/PisReport/Vars/V2212Model.cs
@@ -3,8 +3,8 @@ public class V2212Model
 {
     public string? EmpNumber    { get; set; }="";
     public string? EmpName      { get; set; }="";
-    public DateTime? MovDate    { get; set; }
-    public string? ClName      { get; set; }="";
+    public DateTime? MovDate    { get; set; } 
+    public string? ClName       { get; set; }="";
     public string? Remarks      { get; set; }="";
 
 }
