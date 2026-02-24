da25d9b5bd28dea12306ae0f6dc4d18cf3a4d717
Mar Lopez Magsael <marmagsael@gmail.com>
2026-02-24 11:35:02 +0800

    -

--- HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
+++ HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d19c0eadc58249edbdd9050a810e4f42a10c6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61bd32f16320ae52e1f6e92de6f8663206dff5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ HRMvc/Applications/Vars/V12_102OT.cs
@@ -0,0 +1,21 @@
+using HRApiLibrary.Models._10_Pis;
+
+namespace HRMvc.Applications.Vars;
+
+public class V12_102OT
+{
+    public OtreqhdrModel           OTReqhdr           { get; set; } = new(); 
+    public List<OtreqhdrModel>     OTReqhdrs          { get; set; } = [];
+    public OtreqdtlModel           OTReqdtl           { get; set; } = new(); 
+    public List<OtreqdtlModel>     OTReqdtls          { get; set; } = []; 
+    public bool                     ShowMyPunches       { get; set; } = false; 
+    public bool                     ShowMyReqquest      { get; set; } = false; 
+    public bool                     ShowSendForApproval { get; set; } = false; 
+    public bool                     ShowCancel          { get; set; } = false; 
+
+    
+    public List<Attpunches1Model>   AttPunches1s        { get; set; } = []; 
+    public List<AtttemplateModel>   Atttemplates        { get; set; } = []; 
+    public List<AttreqtypeModel>    Attreqtypes         { get; set; } = []; 
+
+}
