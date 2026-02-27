1454c57df91178bb1aecafbd37343e10e100863d
Abraham Kevin Gagui <abraham_gagui@gervasioph.com>
2026-02-27 10:00:59 +0800

    update FE trdp

--- HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
+++ HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f608fb1f0780233d753c57fc0b9b143c5fa0cf1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d634a5f7a2e790b049ec3ed41c1887d513b2cf7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
