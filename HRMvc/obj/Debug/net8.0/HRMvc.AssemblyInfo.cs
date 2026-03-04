e42476ae65398e5d4806d6784dad62f25e42aeca
Abraham Kevin Gagui <abraham_gagui@gervasioph.com>
2026-03-04 15:13:29 +0800

    update status filter

--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eca05aa5bce460e80622af3bbb9d994798d305e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c6266f561da58f0c80b809e53fc773b35c30a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
