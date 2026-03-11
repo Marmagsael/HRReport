a015da905b15fa7b9893fc809e7aefa2f71ece5d
Mar Lopez Magsael <marmagsael@gmail.com>
2026-03-11 14:47:36 +0800

    -

--- HRApiLibrary/DataAccess/_20_Pay_Report/GPayrollReportDataAccess.cs
+++ HRApiLibrary/DataAccess/_20_Pay_Report/GPayrollReportDataAccess.cs
@@ -28,9 +28,9 @@ public class GPayrollReportDataAccess : IGPayrollReportDataAccess
         return data;
     }
    
-    public async Task<List<RSssPremModel?>?> _02SSSPrem_ByPGrps_ByYYMM(List<string> pgrps, int yyyy, string mm,  string acctType, string opaydb,  string opisdb, string conn)
+    public async Task<List<RSssPremModel?>?> _02SSSPrem_ByPGrps_ByYYMM(List<string> pgrps, int yyyy, string mm,  string opaydb,  string opisdb, string conn)
     {
-        string prd = $"{yyyy.ToString().Trim().Substring(2,2)}"; 
+        string prd = $"{yyyy.ToString().Trim().Substring(2,2)}{mm}"; 
         string sql  = $@"
                             select  t.EmpNumber, e.EmpLastnm, e.EmpFirstNm, e.EmpMidNm, c.ClName Payrollgrp, 
                                     e.DateHired, t.Ee, m.Ecc Cc, m.Er, m.Compensation  from
@@ -42,8 +42,28 @@ public class GPayrollReportDataAccess : IGPayrollReportDataAccess
                                 left join {opisdb}.Client c on c.ClNumber = e.Client_
                                 left join (SELECT * FROM {opaydb}.refssstbl where @Yyyy between yrstart and yrend) m on m.ee = t.ee ";
         var data    = await _sql.FetchData<RSssPremModel?, dynamic>(sql, new { Prd = prd, Pgrps = pgrps, Yyyy = yyyy }, conn);
+        
+        Console.WriteLine($"Count {data.Count}");
         return data;
     }
+
+    public async Task<List<RSssPremModel?>?> _02Tbltran_ByPGrps_ByYYMM(List<string> pgrps, int yyyy, string mm,  string acctType, string opaydb,  string opisdb, string conn)
+    {
+        string prd = $"{yyyy.ToString().Trim().Substring(2,2)}{mm}"; 
+        string sql  = $@"
+                            select  t.EmpNumber, e.EmpLastnm, e.EmpFirstNm, e.EmpMidNm, c.ClName Payrollgrp, 
+                                    e.DateHired, t.Ee, m.Ecc Cc, m.Er, m.Compensation  from
+                                (SELECT EmpNumber, sum(Amount) Ee  FROM {opaydb}.tbltran t
+                                    where AcctNumber = @AcctType and left(trn,4) = @Prd and right(trn,5) in @Pgrps
+                                    group by EmpNumber) t
+
+                                left join {opisdb}.Empmas e on e.empnumber = t.EmpNumber
+                                left join {opisdb}.Client c on c.ClNumber = e.Client_
+                                left join (SELECT * FROM {opaydb}.refssstbl where @Yyyy between yrstart and yrend) m on m.ee = t.ee ";
+        var data    = await _sql.FetchData<RSssPremModel?, dynamic>(sql, new { Prd = prd, Pgrps = pgrps, Yyyy = yyyy, AcctType=acctType }, conn);
+        return data;
+    }
+
 }
 
 
@@ -51,6 +71,7 @@ public interface IGPayrollReportDataAccess
 {
     Task<List<GChartofacctModel?>?>     _02ByAcctTypes(string acctType, string schema, string conn);
     Task<List<GChartofacctModel?>?>     _02s(string schema, string conn);
-    Task<List<RSssPremModel?>?>         _02SSSPrem_ByPGrps_ByYYMM(List<string> pgrps, int yyyy, string mm,  string acctType, string opaydb,  string opisdb, string conn); 
+    Task<List<RSssPremModel?>?>         _02SSSPrem_ByPGrps_ByYYMM(List<string> pgrps, int yyyy, string mm,  string opaydb,  string opisdb, string conn); 
+    Task<List<RSssPremModel?>?> _02Tbltran_ByPGrps_ByYYMM(List<string> pgrps, int yyyy, string mm,  string acctType, string opaydb,  string opisdb, string conn); 
     
 }
