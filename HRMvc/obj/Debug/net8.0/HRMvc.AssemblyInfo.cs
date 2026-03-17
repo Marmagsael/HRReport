2e7c0f30c01054711350b8d86b07aabf1eb5cd1f
judithlorrenreyes <jlrproj@gmail.com>
2026-03-17 12:00:49 +0800

    Add Pagibig and PHIC trdp

--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5f4634ec2214befbdda9677364729f174c3f45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deea62e8666f54e1a956fa2b343df9b19b5b3c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
