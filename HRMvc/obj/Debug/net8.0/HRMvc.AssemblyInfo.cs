ca7c71bd8d3baa33b7f6a3bb78487d2c39efedd4
judithlorrenreyes <jlrproj@gmail.com>
2026-03-19 10:53:56 +0800

    -

--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7c0f30c01054711350b8d86b07aabf1eb5cd1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23b08b065c0f83ae1eb07308c6b6a1deb36df65f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
