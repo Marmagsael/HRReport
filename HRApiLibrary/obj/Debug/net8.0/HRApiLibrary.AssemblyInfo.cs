6a14e41be5e22beffceb83d7cec9918d1b9c298c
Mar Lopez Magsael <marmagsael@gmail.com>
2026-03-20 11:37:26 +0800

    -

--- HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
+++ HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea303e43e79627c8cd56eaa16276b6a09f5cd1cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72fe580dde830e5f6143da7f614e6930460ecc7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HRMvc/Applications/Vars/V12_102Lv.cs
+++ HRMvc/Applications/Vars/V12_102Lv.cs
@@ -5,13 +5,17 @@ namespace HRMvc.Applications.Vars;
 public class V12_102Lv
 {
 
-    public LeaveapplicationModel        LeaveApplication        { get; set; } = new();        
-    public LeaveapplicationModel        LeaveApplicationdtl     { get; set; } = new();  
-    public List<LeavetypeModel>         LeaveTypes              { get; set; } = [];       
-
-    public bool                         ShowMyReqquest          { get; set; } = false; 
-    public bool                         ShowSendForApproval     { get; set; } = false; 
-    public bool                         ShowCancel              { get; set; } = false; 
-    public bool                         ShowLoadTransaction     { get; set; } = false; 
+    public LeaveapplicationModel            LeaveApplication        { get; set; } = new();        
+    public LeaveapplicationdtlModel         LeaveApplicationdtl     { get; set; } = new();  
+    public List<LeaveapplicationdtlModel>   LeaveApplicationdtls    { get; set; } = [];  
+    public List<LeavetypeModel>             LeaveTypes              { get; set; } = [];    
+    public List<TimeoptionModel>?           Hrs                     { get; set; } = [];   
+    public List<TimedurationModel>?         Durations               { get; set; } = [];   
+    public List<string>?                    DutyType               { get; set; } = ["R","RD"];   
+    public AtttemplateModel                 AttTemplate             { get; set; } = new();                  
+    public bool                             ShowMyReqquest          { get; set; } = false; 
+    public bool                             ShowSendForApproval     { get; set; } = false; 
+    public bool                             ShowCancel              { get; set; } = false; 
+    public bool                             ShowLoadTransaction     { get; set; } = false; 
     public string                           Action                  { get; set; } = string.Empty;
 }
