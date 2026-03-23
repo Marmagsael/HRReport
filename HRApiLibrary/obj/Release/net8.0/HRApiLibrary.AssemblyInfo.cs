661b488af86e189feb23638b9a530a40d111cda5
Mar Lopez Magsael <marmagsael@gmail.com>
2026-03-23 10:50:48 +0800

    -

--- HRApiLibrary/obj/Release/net8.0/HRApiLibrary.AssemblyInfo.cs
+++ HRApiLibrary/obj/Release/net8.0/HRApiLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa3dcc60db81ca444212d1c7985599155b086cb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad988f5792ead8ca50897b047e3eec858d4197c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
