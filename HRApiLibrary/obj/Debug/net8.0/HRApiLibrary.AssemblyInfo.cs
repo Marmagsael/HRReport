c08ba522e94f76a13823426dc80d0e15dee3b46b
Mar Lopez Magsael <marmagsael@gmail.com>
2026-04-15 11:44:58 +0800

    p

--- HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
+++ HRApiLibrary/obj/Debug/net8.0/HRApiLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c6396e30d2098e5d1db30ec7bca4981381b80d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc7fe8f694ff3d0b9c0356f0a91e020e4f529694")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
