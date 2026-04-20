44f9df813f2326bde4cbee1c4c70bbd52cdd4446
judithlorrenreyes <jlrproj@gmail.com>
2026-04-20 19:20:11 +0800

    -

--- HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
+++ HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f40832ca8751a62e5d53e3e755390b9c2eef07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b22e3adf1a1e5c414da0e751c991abc745cbd573")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
