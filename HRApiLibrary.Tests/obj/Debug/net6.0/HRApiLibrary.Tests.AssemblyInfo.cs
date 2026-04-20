882e0abb0b06129a1cf4856e4003cce5bcf98087
judithlorrenreyes <jlrproj@gmail.com>
2026-04-20 15:45:31 +0800

    Created Employee Entry modulr for 201 report

--- HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
+++ HRApiLibrary.Tests/obj/Debug/net6.0/HRApiLibrary.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e75e220554b6b53b689bb03707b5cfe5469f9b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f40832ca8751a62e5d53e3e755390b9c2eef07")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApiLibrary.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
