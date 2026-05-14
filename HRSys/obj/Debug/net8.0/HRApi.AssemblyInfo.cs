b4e9f3287f553262dff702c8617bca514109fb72
Mar Lopez Magsael <marmagsael@gmail.com>
2026-05-14 11:25:45 +0800

    -

--- HRSys/obj/Debug/net8.0/HRApi.AssemblyInfo.cs
+++ HRSys/obj/Debug/net8.0/HRApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f9198dd1a5a7e9cd47ac0030d6b695e14e8b099")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0411571681f473eb33067c50f5e7d7afd85f41")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
