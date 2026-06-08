dfd162de26819f55e2ecc80d3b22cedc4da59ca0
judithlorrenreyes <jlrproj@gmail.com>
2026-06-08 18:42:20 +0800

    Add Reports

---
+++ HRMvc/Applications/PisReport/Vars/V2212Model.cs
@@ -0,0 +1,22 @@
+namespace HRMvc.Applications.PisReport.Vars;
+
+public class V2212Model
+{
+    public string? Action { get; set; } = "";
+    public List<R2212Model> RepDtls { get; set; } = [];
+}
+
+
+public class R2212Model
+{
+    public string? EmpNumber    { get; set; }="";
+    public string? EmpName      { get; set; }="";
+    public DateTime? MovDate    { get; set; } 
+    public string? ClName       { get; set; }="";
+    public string? Remarks      { get; set; }="";
+
+    // Added -----------------------------------
+    public string? EmpLastNm { get; set; } = "";
+    public string? EmpFirstNm { get; set; } = "";
+
+}
--- HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
+++ HRMvc/obj/Debug/net8.0/HRMvc.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46164f620a261f2038f46e24d3712f36676c072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a7455bfd1cda066401e8a9cc2b519392fc21c1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HRMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
