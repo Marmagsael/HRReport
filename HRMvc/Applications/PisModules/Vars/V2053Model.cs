056222a03653f471c396642e3f08125af5e13980
judithlorrenreyes <jlrproj@gmail.com>
2026-09-03 23:31:03 +0800

    Update Client Entry Design

---
+++ HRMvc/Applications/PisModules/Vars/V2053Model.cs
@@ -0,0 +1,16 @@
+﻿using HRApiLibrary.Models._10_Pis.OPis;
+
+namespace HRMvc.Applications.PisModules.Vars
+{
+    public class V2053Model
+    {
+        public string? OPisdb       { get; set; } = string.Empty;
+        public string? Maindb       { get; set; } = string.Empty;
+        public string? Mainpisdb    { get; set; } = string.Empty;
+        public string? Conn         { get; set; } = string.Empty;
+
+        public bool ShowClientForm  {  get; set; } = false;
+        public List<OClientModel?>? Clients { get; set; } = [];
+        public OClientModel?        Client  { get; set; } = new();
+    }
+}
